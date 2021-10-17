8350c68d2f42056380826f3cd06e42606a15615f
NoahZinter <noah.zinter@gmail.com>
2021-10-17 10:38:35 -0600

    update webpacker, edit webpack config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')('./app/javascript/css/tailwind.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
