e1b30e08d06520f2b1f45f1db02fb30c6693124f
NoahZinter <noah.zinter@gmail.com>
2021-10-17 13:57:25 -0600

    require autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    require('autoprefixer'),
     require('tailwindcss')('tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
